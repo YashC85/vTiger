add3320c0bda074c2b3043e30b4372fcb193e79b
PC <PC@PC>
2021-07-09 23:45:22 +0530

    Pushing first Framework

---
+++ src/test/java/com/crm/vTiger/OrganisationTest/CreateOrganisation.java
@@ -0,0 +1,12 @@
+package com.crm.vTiger.OrganisationTest;
+
+import org.testng.annotations.Test;
+
+public class CreateOrganisation {
+	@Test
+	
+	public void createOrgnisation() {
+		System.out.println("Creating Organisation...");
+	}
+
+}
